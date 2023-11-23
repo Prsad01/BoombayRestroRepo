4e78ae90b987c6e6287ead9058eef9dd26fa0ce3
unknown <prasadmande01@gmail.com>
2023-11-23 13:40:09 +0530

    creating serailizer, views, urls for category and making API's

--- BoombayRestro/Restro/views.py
+++ BoombayRestro/Restro/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render ,HttpResponse
+from rest_framework import viewsets
+from .serializer import categorySerializer
+from .models import category
 
-# Create your views here.
+
 
 def index(request):
     return HttpResponse("Hi Pooja")
+
+class categoryView(viewsets.ModelViewSet):
+    serializer_class = categorySerializer
+    queryset = category.objects.all()
